8f58ad011714c7a4588e88583e48825a99e29a3e
ss1234 <ss450555@gmail.com>
2021-03-05 13:28:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -241,6 +241,7 @@ client.on("message", (msg) => {
   if (
     msg.author.id === "385840144273506307" &&
     (msg.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes("rhino") ||
+        msg.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes("rh1no") || 
       msg.content.includes("coochie") ||
       msg.content.includes("ewwww-lol.gif") ||
       msg.content.includes("ace-ventura"))
